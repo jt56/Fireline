57ea34420cbe0b0f585a9501c0c6e40200cfa3fb
James Thompson <jsthompson@umail.ucsb.edu>
2017-01-16 21:46:51 -0800

    Added comments and units to layout

--- app/src/main/java/com/binx/fireline/adapter/IncidentAdapter.java
+++ app/src/main/java/com/binx/fireline/adapter/IncidentAdapter.java
@@ -3,7 +3,6 @@ package com.binx.fireline.adapter;
 import android.content.Context;
 import android.graphics.Color;
 import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,13 +13,13 @@ import android.widget.TextView;
 
 import com.binx.fireline.R;
 import com.binx.fireline.model.Incident;
-import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 
 /**
  * Created by james on 1/5/17.
+ * Incident Adapter
  */
 
 public class IncidentAdapter extends ArrayAdapter<Incident> {
@@ -54,11 +53,17 @@ public class IncidentAdapter extends ArrayAdapter<Incident> {
 
         Incident item = getItem(position);
 
+        vh.imageView.setImageResource(new IncidentImageAdapter().getIncidentTypeImageId(item.getIncidentType()));
         vh.textViewIncidentType.setText(item.getIncidentType());
         vh.textViewStatus.setText(item.getStatus());
         vh.textViewResponseDate.setText(item.getResponseDate());
         vh.textViewBlockAddrressCity.setText(item.getBlock() + " " + item.getAddress() + ", " + item.getCity());
-        vh.imageView.setImageResource(new IncidentImageAdapter().getIncidentTypeImageId(item.getIncidentType()));
+        vh.textViewUnits.setText(item.getUnits());
+        if (item.getComment().equals("")){
+            vh.textViewComment.setVisibility(View.GONE);
+        } else {
+            vh.textViewComment.setText(item.getComment());
+        }
 
         if (position % 2 == 1) {
             vh.rootView.setBackgroundColor(Color.LTGRAY);
@@ -75,14 +80,25 @@ public class IncidentAdapter extends ArrayAdapter<Incident> {
         public final TextView textViewStatus;
         public final TextView textViewResponseDate;
         public final TextView textViewBlockAddrressCity;
+        public final TextView textViewUnits;
+        public final TextView textViewComment;
 
-        private ViewHolder(RelativeLayout rootView, ImageView imageView, TextView textViewIncidentType, TextView textViewStatus, TextView textViewResponseDate, TextView textViewBlockAddrressCity) {
+        private ViewHolder(RelativeLayout rootView,
+                           ImageView imageView,
+                           TextView textViewIncidentType,
+                           TextView textViewStatus,
+                           TextView textViewResponseDate,
+                           TextView textViewBlockAddrressCity,
+                           TextView textViewUnits,
+                           TextView textViewComment) {
             this.rootView = rootView;
             this.imageView = imageView;
             this.textViewIncidentType = textViewIncidentType;
             this.textViewStatus = textViewStatus;
             this.textViewResponseDate = textViewResponseDate;
             this.textViewBlockAddrressCity = textViewBlockAddrressCity;
+            this.textViewUnits = textViewUnits;
+            this.textViewComment = textViewComment;
         }
 
         public static ViewHolder create(RelativeLayout rootView) {
@@ -91,7 +107,9 @@ public class IncidentAdapter extends ArrayAdapter<Incident> {
             TextView textViewStatus = (TextView) rootView.findViewById(R.id.textViewStatus);
             TextView textViewResponseDate = (TextView) rootView.findViewById(R.id.textViewResponseDate);
             TextView textViewBlockAddrressCity = (TextView) rootView.findViewById(R.id.textViewBlockAddrressCity);
-            return new ViewHolder(rootView, imageView, textViewIncidentType, textViewStatus, textViewResponseDate, textViewBlockAddrressCity);
+            TextView textViewUnits = (TextView) rootView.findViewById(R.id.textViewUnits);
+            TextView textViewComment = (TextView) rootView.findViewById(R.id.textViewComment);
+            return new ViewHolder(rootView, imageView, textViewIncidentType, textViewStatus, textViewResponseDate, textViewBlockAddrressCity, textViewUnits, textViewComment);
         }
     }
 }
